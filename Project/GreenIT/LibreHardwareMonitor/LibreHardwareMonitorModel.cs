0597614188c0d411f2f64ebb8c103bdcb1711e57
Antoine ROBIN <antoine.robin@factorfx.com>
2023-11-15 17:09:09 +0100

    fix(LibreHardwareMonitor): float format

--- Project/GreenIT/LibreHardwareMonitor/LibreHardwareMonitorModel.cs
+++ Project/GreenIT/LibreHardwareMonitor/LibreHardwareMonitorModel.cs
@@ -24,7 +24,7 @@ namespace GreenIT.LibreHardwareMonitor
                         if (sensor.Value > 50000) consumption.Add("CONSUMPTION", "VM detected");
                         else
                         {
-                            consumption.Add("CONSUMPTION", sensor.Value.ToString());
+                            consumption.Add("CONSUMPTION", sensor.Value.ToString().Replace(",", "."));
                         }
                     }
                 }
